3f1a90a27040aaa75cea2fbb610bcfa8bb21831d
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-05-05 17:29:43 -0300

    feat(back-end): banco de dados criado

---
+++ back-end/src/main/java/com/hublast/api_blog/ApiBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hublast.api_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBlogApplication.class, args);
+	}
+
+}
